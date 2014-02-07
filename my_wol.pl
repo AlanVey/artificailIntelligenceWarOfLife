5686f7b4d184552bdb943184e98657a43a15cfe5
Alan Vey <amv12@ic.ac.uk>
2014-02-07 20:43:24 +0000

    Method stubs added

---
+++ my_wol.pl
@@ -0,0 +1,4 @@
+bloodlust(+PlayerColour, +CurrentBoardState, -NewBoardState, -Move). 
+self_preservation(+PlayerColour, +CurrentBoardState, -NewBoardState, -Move). 
+land_grab(+PlayerColour, +CurrentBoardState, -NewBoardState, -Move). 
+minimax(+PlayerColour, +CurrentBoardState, -NewBoardState, -Move).
